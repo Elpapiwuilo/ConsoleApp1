20864fa2f5fd510d698e7f45693119bf4346292f
jopatino <jopatino86@misena.edu.co>
2019-04-08 17:51:17 -0500

    ejecicios

---
+++ EjercicioDos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EjercicioDos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicioTwo();
+
+        }
+
+        public static void ejercicioTwo()
+        {
+          
+
+            Console.WriteLine("Escribe un numero entero");
+            var enteroone = 0;
+            enteroone = Convert.ToInt32(Console.ReadLine());
+
+            if (enteroone > 0 )
+            {
+                Console.WriteLine("Es un número positivo");
+
+            }else if (enteroone<0)
+            {
+                Console.WriteLine($"Es un número positivo: {enteroone * -1}");
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
